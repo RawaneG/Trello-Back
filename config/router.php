aa629b0301e101c7b94884ac1782d5804d16ef80
Rawane <rawane.meissa@gmail.com>
2022-04-04 06:30:36 +0000

    Projet Back Trello

---
+++ config/router.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_REQUEST['controller']))
+{
+    switch ($_REQUEST['controller'])
+    {
+        case "tache" :
+        require_once(PATH_SRC."controllers/tache.controllers.php");
+        break;
+    }
+}
+else
+{
+    require_once(PATH_SRC."controllers/tache.controllers.php");
+}
