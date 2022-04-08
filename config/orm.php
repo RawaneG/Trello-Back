81d24dd652da9d0776862a4e9e9d6e0b07fecec5
Rawane <rawane.meissa@gmail.com>
2022-04-08 18:45:27 +0000

    update

--- config/orm.php
+++ config/orm.php
@@ -1,21 +1,16 @@
 <?php
-///Recuperation des donnees du fichier
+
 function findStates()
 {
     $dataJson = file_get_contents(PATH_DB);
     return $dataJson;
 }
-//Enregistrement et Mis a jour des donnees du fichier
+
 function addEtat($key,$newTask)
 {
-    // ici on récupere le contenu du ficher json sous format string
     $dataJson = file_get_contents(PATH_DB);
-    // ici on le transforme en tableau
     $data = json_decode($dataJson,true);
-    // on ajoute le nouveau utilisateur
     $data[$key][] = $newTask;
-    // pour qu'il retuourne le ficher en string(donneé)
     $fileNameData = json_encode($data);
-    // et on retour les données en string
     file_put_contents(PATH_DB, $fileNameData);
 }
---
+++ src/controllers/tache.controllers.php
@@ -0,0 +1,23 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "GET")
+{
+    if(isset($_REQUEST['action']))
+    {
+        if($_REQUEST['action'] == 'restauration')
+        {
+            echo findStates();
+        }   
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    if(isset($_REQUEST['action']))
+    {
+        if($_REQUEST['action'] == 'ajouter')
+        {
+            $data = json_decode(file_get_contents('php://input'),true);
+            $actual_time = date("Y-m-d");
+            addEtat($actual_time, $data);
+        }   
+    }
+}
